b8cf744ff018722b8b6796e02eabf5a22acf9127
Lipeng Guo <guolipeng@smartxtruck.com>
2024-01-10 16:31:17 +0800

    add vitejs react plugin

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,7 @@
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+    plugins: [react()]
+});
